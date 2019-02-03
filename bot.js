02ccaa84062877b2baf84071f30f4c5b9d9dde00
arminnaami <40029401+arminnaami@users.noreply.github.com>
2019-02-03 16:34:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,38 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const activities_list = [
+    "https://discord.gg/HSNEcyj", 
+    "Hi,Discord:HSNEcyj",
+    "Armin", 
+    "!help Vampire", 
+    "Gaming Mode", 
+    "Persian GAP", 
+    "!invite", 
+    "https://discord.gg/HSNEcyj"
+    ]; // creates an arraylist containing phrases you want your bot to switch through.
+
+client.on('ready', () => {
+    setInterval(() => {
+        const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
+        client.user.setActivity(activities_list[index]); // sets bot's activities to one of the phrases in the arraylist.
+    }, 5000); // Runs this every 10 seconds.
+});
+
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === 'ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === '!invite') {
+    msg.reply('https://discord.gg/HSNEcyj');
+  }
+});
+
+client.on('message', msg => {
+  if (msg.content === '!help') {
+    msg.reply('https://discord.gg/HSNEcyj');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
